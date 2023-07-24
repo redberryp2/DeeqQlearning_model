3ca6381747bea5ccb242f29a3970dffa04d09747
sangwoo <sangwoo6999@naver.com>
2023-07-24 22:38:06 +0900

    first commit

---
+++ self_driving_agent/utils.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+import pygame
+import math
+import numpy as np
+
+def process_img(image, dim_x=128, dim_y=128):
+    array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+    array = np.reshape(array, (image.height, image.width, 4))
+    array = array[:, :, :3]
+    array = array[:, :, ::-1]
+
+    dim = (dim_x, dim_y)
+    resized_img = cv2.resize(array, dim, interpolation=cv2.INTER_AREA)
+    img_gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+    scaledImg = img_gray/255.
+
+
+    mean, std = 0.5, 0.5
+    normalizedImg = (scaledImg - mean) / std
+
+    return normalizedImg
+
+def draw_image(surface, image, blend=False):
+    array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+    array = np.reshape(array, (image.height, image.width, 4))
+    array = array[:, :, :3]
+    array = array[:, :, ::-1]
+    image_surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+    if blend:
+        image_surface.set_alpha(100)
+    surface.blit(image_surface, (0, 0))
+
+def get_font():
+    fonts = [x for x in pygame.font.get_fonts()]
+    default_font = 'ubuntumono'
+    font = default_font if default_font in fonts else fonts[0]
+    font = pygame.font.match_font(font)
+    return pygame.font.Font(font, 14)
+
+def should_quit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                return True
+    return False
+
+def get_speed(vehicle):
+    vel = vehicle.get_velocity()
+
+    return 3.6 * math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2)
+
+def correct_yaw(x):
+    return(((x%360) + 360) % 360)
+
+def create_folders(folder_names):
+    for directory in folder_names:
+        if not os.path.exists(directory):
+                os.makedirs(directory)
